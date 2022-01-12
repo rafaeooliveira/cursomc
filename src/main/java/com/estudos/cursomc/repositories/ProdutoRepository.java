bab98f2b4377f393e118e36e04f3490ffcd42a9e
rafaeooliveira <rafaell.oliveira001@gmail.com>
2022-01-12 13:46:20 -0300

    Produto e associacao muito para muitos

---
+++ src/main/java/com/estudos/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.estudos.cursomc.repositories;
+
+import com.estudos.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
