e17653a215d533566978e97291c4d697713f6fd9
rafaeooliveira <rafaell.oliveira001@gmail.com>
2022-01-13 16:31:48 -0300

    Pedido, EstadoPagamento, e Pagamento

---
+++ src/main/java/com/estudos/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.estudos.cursomc.repositories;
+
+import com.estudos.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+}
