400a493f6482afa051826f74b9e5e05bdc96bcad
rafaeooliveira <rafaell.oliveira001@gmail.com>
2022-01-13 17:46:34 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/estudos/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.estudos.cursomc.repositories;
+
+import com.estudos.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+}
